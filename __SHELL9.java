40894666a07d84ae20c909fbbb549abddbd96478
cristobal <cyarari@gmail.com>
2020-12-01 09:00:14 +0100

    01 - Se pueden mostrar todas las canciones

---
+++ __SHELL9.java
@@ -0,0 +1,10 @@
+
+public class __SHELL9 extends bluej.runtime.Shell {
+public static void run() throws Throwable {
+final bluej.runtime.BJMap __bluej_runtime_scope = getScope("C:\\Users\\Alumno\\Documents\\music-organizer-v2");
+final MusicOrganizer musicOrg1 = (MusicOrganizer)__bluej_runtime_scope.get("musicOrg1");
+
+
+musicOrg1.listAllFiles();
+
+}}
